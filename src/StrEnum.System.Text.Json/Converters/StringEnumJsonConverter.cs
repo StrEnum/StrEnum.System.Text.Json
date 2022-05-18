a5009884437b635ef0d225423b910522aa729de6
Dmitry Khmara <dmitry.khmara@gmail.com>
2022-05-18 19:50:26 +0300

    Add a JSON converter, a converter factory, and an extension method to set everything up. (#1)

---
+++ src/StrEnum.System.Text.Json/Converters/StringEnumJsonConverter.cs
@@ -0,0 +1,30 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace StrEnum.System.Text.Json.Converters;
+
+/// <summary>
+/// Allows to serialize and deserialize string enums.
+/// </summary>
+/// <typeparam name="TStringEnum"></typeparam>
+public class StringEnumJsonConverter<TStringEnum>: JsonConverter<TStringEnum> where TStringEnum: StringEnum<TStringEnum>, new()
+{
+    public override TStringEnum Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+    {
+        var jsonValue = reader.GetString();
+
+        StringEnum<TStringEnum>.TryParse(reader.GetString()!, out var parsed);
+
+        if (parsed == null || !((string)parsed).Equals(jsonValue, StringComparison.InvariantCulture))
+        {
+            throw new JsonException($"Requested name or value '{jsonValue}' was not found in the string enum '{typeof(TStringEnum).Name}'.");
+        }
+
+        return parsed;
+    }
+
+    public override void Write(Utf8JsonWriter writer, TStringEnum value, JsonSerializerOptions options)
+    {
+        writer.WriteStringValue((string)value);
+    }
+}
---
+++ src/StrEnum.System.Text.Json/JsonSerializerOptionsExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System.Text.Json;
+using StrEnum.System.Text.Json.Converters;
+
+namespace StrEnum.System.Text.Json
+{
+    public static class JsonSerializerOptionsExtensions
+    {
+        /// <summary>
+        /// Configure System.Text.Json to serialize and deserialize string enums.
+        /// </summary>
+        /// <param name="options"></param>
+        /// <returns></returns>
+        public static JsonSerializerOptions UseStringEnums(this JsonSerializerOptions options)
+        {
+            options.Converters.Add(new StringEnumJsonConverterFactory());
+            
+            return options;
+        }
+    }
+}
